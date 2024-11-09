0308ca72b12652efe9923ba4f62b75d286c70dd7
ItsAltoo <Itsmalik57@gmail.com>
2024-11-09 22:09:27 +0800

    9.11.24

--- Day 6/script.js
+++ Day 6/script.js
@@ -9,3 +9,31 @@ Tombol untuk memilih operasi (+, -, *, /).
 Menampilkan hasil perhitungan.
 Tambahan CSS: Desain tombol dan input agar terlihat menarik.
 */
+
+const count = (operasi) => {
+  const num1 = parseFloat(document.getElementById("inpt1").value);
+  const num2 = parseFloat(document.getElementById("inpt2").value);
+  let result;
+
+  if (isNaN(num1) || isNaN(num2)) {
+    result = "Input yang anda taruh tidak valid !";
+  } else {
+    switch (operasi) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num2 !== 0 ? num1 / num2 : "Tidak dapat membagi dengan 0!";
+        break;
+      default:
+        result = "Operasi tidak valid!";
+    }
+  }
+  document.getElementById("result").textContent = result;
+};
