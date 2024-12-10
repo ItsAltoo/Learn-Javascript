98da8ddb1da8758d4b2cbcc8375de67073577f84
ItsAltoo <Itsmalik57@gmail.com>
2024-12-10 20:33:09 +0800

    10.12.24

--- OOP/OOP4.js
+++ OOP/OOP4.js
@@ -1,18 +1,33 @@
 class Buku {
+  constructor(judul, pengarang, tahun_terbit) {
+    this.judul = judul;
+    this.pengarang = pengarang;
+    this.terbit = tahun_terbit;
+  }
 
-    constructor (judul,pengarang,tahun_terbit){
-        this.judul = judul
-        this.pengarang = pengarang
-        this.terbit = tahun_terbit
-    }
-
-    deskripsi = () => {
-        console.log(`judul : ${this.judul}\npengarang : ${this.pengarang}\nTahun terbit : ${this.terbit}`);
-    }
+  deskripsi = () => {
+    console.log(
+      `judul : ${this.judul}\npengarang : ${this.pengarang}\nTahun terbit : ${this.terbit}`
+    );
+  };
 }
 
-class Mahasiswa extends Buku
+class Mahasiswa extends Buku {
+  constructor(namaMhs, nim) {
+    super(this.judul);
+    this.nama = namaMhs;
+    this.nim = nim;
+    this.bukuDipinjam = []
+  }
 
+  pinjamBuku = () => {
+    this.bukuDipinjam.push(this.judul)
+  }
 
-let buku = new Buku('buku','saya',123)
-buku.deskripsi()
+  simpanFile = () => {
+
+  }
+}
+
+let buku = new Buku("buku", "saya", 123);
+buku.deskripsi();
